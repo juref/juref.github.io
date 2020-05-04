27d9f0ece884022292d84cb7216d3699fe0eb952
juref <kreativc@rh2.neoserv.si>
2020-05-05 00:00:37 +0200

    new timing + no cursor

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
   loop: false,
-  delay: 50,
+  delay: 20,
   autoStart: true,
   cursor: '█',
   strings: []
@@ -10,29 +10,29 @@ var typewriter = new Typewriter(app, {
 
 typewriter
   .typeString('Hello, my name is Jure Filipič.')
-  .pauseFor(1000)
-  .typeString('<br />')
-  .pauseFor(300)
-  .typeString('<br />')
   .pauseFor(500)
+  .typeString('<br />')
+  .pauseFor(200)
+  .typeString('<br />')
+  .pauseFor(200)
   .typeString('I work for <strong><a href="https://gigodesign.com" class="underline">[GIGODESIGN]</a></strong> as a media designer, junior web developer, and system administrator.')
-  .pauseFor(500)
+  .pauseFor(200)
   .typeString('<br />')
-  .pauseFor(600)
+  .pauseFor(400)
   .typeString('<br />')
-  .pauseFor(800)
+  .pauseFor(400)
   .typeString('I am an experienced Media Designer with over 20 years of history working in the design industry.')
-  .pauseFor(600)
+  .pauseFor(200)
   .typeString(' With excellent knowledge of OS X and Linux systems, networks and hosting')
-  .pauseFor(900)
-  .typeString(', and basic knowledge of Python, JavaScript, PHP, CSS and HTML.')
-  .pauseFor(800)
-  .typeString('<br />')
   .pauseFor(500)
+  .typeString(', and basic knowledge of Python, JavaScript, PHP, CSS and HTML.')
+  .pauseFor(400)
   .typeString('<br />')
-  .pauseFor(300)
+  .pauseFor(200)
+  .typeString('<br />')
+  .pauseFor(100)
   .typeString('You can find me online: <br />')
-  .pauseFor(1000)
+  .pauseFor(500)
   .typeString('<a href="mailto:jure@kreativc.si" class="underline" target="_blank">Email me</a> <br />')
   .typeString('<a href="https://www.linkedin.com/in/jure-filipic" class="underline" target="_blank">LinkedIn</a> <br />')
-  .pauseFor(1000)
+  .pauseFor(500)
